7f5ca8a2876f59e18a567c1f702ae619b1b693d9
nouraa253 <156623201+nouraa253@users.noreply.github.com>
2025-09-28 13:11:40 +0300

    Update customer.service.spec.ts

--- frontend/src/app/customer.service.spec.ts
+++ frontend/src/app/customer.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CustomerService } from './customer.service';
 
 describe('CustomerService', () => {
   let service: CustomerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CustomerService]
+    });
     service = TestBed.inject(CustomerService);
   });
 
