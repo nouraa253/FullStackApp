9d323a201351bc6f5fbda6e357968519fcb4ed3b
nouraa253 <156623201+nouraa253@users.noreply.github.com>
2025-10-02 04:51:49 +0300

    Update CustomerServiceImpl.java

--- demo/src/main/java/com/example/demo/serviceImpl/CustomerServiceImpl.java
+++ demo/src/main/java/com/example/demo/serviceImpl/CustomerServiceImpl.java
@@ -5,7 +5,7 @@ import com.example.demo.repo.CustomerRepository;
 import com.example.demo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.context.annotation.Profile;   // ← أضف هذا الاستيراد
+import org.springframework.context.annotation.Profile;   
 
 
 
